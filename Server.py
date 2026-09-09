fa10a14003af97f87c7a1db584f7516541655aba
adamsashiruma-netizen <adamsashiruma@gmail.com>
2026-09-09 16:21:16 +0300

    Create Server.py

---
+++ Server.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    data = request.json
+    print("M-PESA CALLBACK:")
+    print(data)
+    return {"ResultCode": 0, "ResultDesc": "Accepted"}
+
+@app.route("/")
+def home():
+    return "M-PESA callback server is running"
+
+app.run(host="0.0.0.0", port=10000)
